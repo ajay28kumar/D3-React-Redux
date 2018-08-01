3fdd1b0566591f09b65ed420bc641ba800175f59
ajay28kumar <krajay015@gmail.com>
2018-08-01 11:47:43 +0530

    [WIP] - added logger middleware

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 // import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
@@ -6,13 +6,19 @@ import reducers from './reducers';
 const logger = ({ getState }) => next => action => {
   // Call the next dispatch method in the middleware chain.
   const returnValue = next(action);
-  console.log('state after dispatch', getState(), action.type);
+  const payload = { actionType: action.type, state: getState() };
+  console.log('state after dispatch', payload);
   // This will likely be the action itself, unless
   // a middleware further in chain changed it.
   return returnValue;
 };
 const middleWares = [thunk, logger];
-const configureStore = initialState => createStore(combineReducers(reducers), (initialState = {}), applyMiddleware(...middleWares));
+const configureStore = initialState =>
+  createStore(
+    combineReducers(reducers),
+    (initialState = {}),
+    compose(applyMiddleware(...middleWares), window.devToolExtension ? window.devToolExtension() : f => f),
+  );
 
 const store = configureStore();
 
